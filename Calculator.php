9e914e2079f6f7988edb560dbd9558427e7b36a3
Himanshu Bhaisare <himanshu_bhaisare@hotmail.com>
2015-06-20 17:12:22 -0700

    Evaluate method on calculator

--- Calculator.php
+++ Calculator.php
@@ -43,4 +43,84 @@ class Calculator {
     public function divide($dividend, $divisor) {
         return $dividend / $divisor;
     }
+
+    /**
+     * @param $a
+     * @param $b
+     * @return int
+     */
+    public function modulus($a, $b) {
+        return $a % $b;
+    }
+
+    /**
+     * @param $a
+     * @param $b
+     * @return mixed
+     */
+    public function exponentiation($a, $b) {
+        return $a ** $b; // introduced in php 5.6
+    }
+
+    /**
+     * Evaluate combination of binary operations
+     *
+     * @param array $numbers stores all the numbers in the expression in order
+     * @param array $operators stores all the operators in the expression in order
+     * @throws Exception
+     * @return int
+     */
+    public function evaluate(array $numbers, array $operators) {
+        if(empty($numbers) || empty($operators)) {
+            throw new Exception("Cannot calculate");
+        }
+
+        $numberCount = count($numbers);
+        // start with 0
+        $result = 0;
+        // add this zero to the first number in numbers array
+        array_push($operators, '+');
+
+        for($i = 0; $i < $numberCount; $i++) {
+            $operator = array_pop($operators);
+            $operation = $this->getOperation($operator);
+            $result = $this->$operation($numbers[$i], $result);
+        }
+
+        return $result;
+    }
+
+    /**
+     * Read operator and return respective operation method
+     *
+     * @param $operator
+     * @return string
+     * @throws Exception
+     */
+    public function getOperation($operator) {
+        switch($operator) {
+            case '+' :
+                $operation = 'add';
+                break;
+            case '-' :
+                $operation = 'subtract';
+                break;
+            case '*' :
+                $operation = 'multiply';
+                break;
+            case '/' :
+                $operation = 'divide';
+                break;
+            case '%' :
+                $operation = 'modulus';
+                break;
+            case '^' :
+                $operation = 'exponentiation';
+                break;
+            default :
+                throw new Exception("Calculator does not support this operator");
+        }
+
+        return $operation;
+    }
 }
---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: himanshubhaisare
+ * Date: 6/20/15
+ * Time: 4:29 PM
+ *
+ * This script performs calculations using the calculator object
+ */
+
+require_once 'Calculator.php';
+
+$calculator = new Calculator();
+
+// Simple Addition;
+$addition = $calculator->add(6, 5);
+echo "\nAddition of 6 and 5 = $addition"; // prints 11
+
+// Simple Subtraction
+$subtraction = $calculator->subtract(6, 5);
+echo "\nSubtraction of 6 and 5 = $subtraction"; // prints 1
+
+// Simple multiplication
+$multiplication = $calculator->multiply(6, 5);
+echo "\nMultiplication of 6 and 5 = $multiplication"; // prints 30
+
+// Simple division
+$division = $calculator->divide(10, 2);
+echo "\nDivision of 10 by 2 = $division"; //prints 5
+
+// Simple Modulo operation
+$modulus = $calculator->modulus(6, 5);
+echo "\nModulus of 6 by 5 = $modulus"; //prints 1
+
+// Simple exponentiation
+$exponentiation = $calculator->exponentiation(10, 2);
+echo "\nExponentiation of 10 by 2 = $exponentiation"; //prints 100
+
+// combination of binary operation 1 + 1 - 4 * 4
+$result = $calculator->evaluate($numbers, $operators);
+echo "\n 1 + 1 - 4 * 4 = $result";
